2bd14383004f84e507f8d5742fa8fe0ad48d3052
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-03-15 21:49:30 +0900

    feat : NavController 적용 및 바텀네비 구현

---
+++ presentation/src/main/java/com/bendeng/presentation/ui/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.bendeng.presentation.ui
+
+import com.bendeng.presentation.R
+import com.bendeng.presentation.base.BaseFragment
+import com.bendeng.presentation.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun initView() {
+    }
+
+    override fun initEventObserver() {
+    }
+}
